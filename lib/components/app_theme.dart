e4e5734266a5f9ceb26244b1ae5e852b7c60b7e9
victorzarzar <victorzarzar58@gmail.com>
2024-03-04 10:32:16 -0300

    first commit

---
+++ lib/components/app_theme.dart
@@ -0,0 +1,16 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static Color get primaryColor => const Color(0xFFFFFFFF);
+
+  static Color get secondaryColor => const Color(0x00000000);
+}
+
+class BackGround {
+  BackGround._();
+
+  static Color get primaryColor => const Color(0xFFF5F5F5);
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app_flutter/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shopping Flutter App',
+      theme: ThemeData(
+        useMaterial3: false,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
